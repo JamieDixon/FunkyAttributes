4de49c2c174b5f691fbea63e187e12056f4b3368
Jamie Dixon <jamie@jamie-dixon.co.uk>
2014-03-13 12:46:16 +0000

    added console app to test func attribute

---
+++ FuncAttributes/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace FuncAttributes
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PartialCaller(CachedMethod);
+        }
+
+        public static void PartialCaller(Func<string, string> func)
+        {
+            var isAttrDefined = func.Method.IsDefined(typeof (CustomAttribute), false);
+
+            Console.Write("Does our method have the right attribute: {0}", isAttrDefined);
+
+            Console.ReadLine();
+        }
+
+        [Custom]
+        public static string CachedMethod(string args)
+        {
+            return "Chicken dippers";
+        }
+    }
+
+    public class CustomAttribute : Attribute
+    {
+        
+    }
+}
